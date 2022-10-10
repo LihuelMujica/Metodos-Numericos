f38b966acd10003869fbfdc5f530affe511dfdbc
LihuelMujica <lihuelmujica@gmail.com>
2022-10-10 19:57:49 -0300

    ejercicio 16 guia 0

--- 0. Introduccion octave/dados.m
+++ 0. Introduccion octave/dados.m
@@ -11,12 +11,3 @@ c=1;
     c= c+1;
     end
 endfunction
-function [d]=dadosm(n)
-    i=1;
-    prom=0;
-    while i<=1000
-        prom=prom+dados(n);
-        i=i+1;
-    endwhile
-    prom=prom/1000
-endfunction
---
+++ 0. Introduccion octave/ej16.m
@@ -0,0 +1,9 @@
+t = 0:1:84;
+v = load('./0. Introduccion octave/radar.txt')';
+plot(t,v,'*');
+
+minAprox = 87
+maxAprox = 170.4
+
+min=min(v)
+max=max(v)
